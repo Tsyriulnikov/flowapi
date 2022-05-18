fb62cce18e968ce3d177055a76cce5ba9ff9bec7
Tsyriulnikov <max_djvu@mail.ru>
2022-05-18 13:32:59 +0300

    flowapi v.0.2

--- src/components/api.tsx
+++ src/components/api.tsx
@@ -1,14 +1,17 @@
 import axios from "axios";
 
-const axiosInstanse =axios.create({
-
+const axiosInstanse = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {'API-KEY': 'd0ce4cd8-d0d2-4152-99ad-f6e1407cb23f'}
 })
 
-export const api = {
-    loadItems(categoryId:any) {
-return axiosInstanse.get('/items' + categoryId)
-    },
-    deleteItem(id:any) {
-return axiosInstanse.delete('/items'+id)
-    }
-}
+
+// export const api = {
+//     loadItems(categoryId:any) {
+// return axiosInstanse.get('/items' + categoryId)
+//     },
+//     deleteItem(id:any) {
+// return axiosInstanse.delete('/items'+id)
+//     }
+// }
